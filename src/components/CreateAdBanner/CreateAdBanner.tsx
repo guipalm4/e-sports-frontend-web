201107f2a6129811c8d406601423457cec67f37c
guipalm4 <gomes.lmc@gmail.com>
2022-09-18 20:02:24 -0300

    Create components GameBanner + CreateAdBanner and connect with API

---
+++ src/components/CreateAdBanner/CreateAdBanner.tsx
@@ -0,0 +1,22 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
+
+function CreateAdBanner() {
+  return (
+    <div className='pt-1 bg-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-white text-2xl font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anuncio para encontar novos players</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3 '>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+    </div>
+  )
+}
+
+export default CreateAdBanner
